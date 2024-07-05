5e19b9f04ef670f57c38ee3a94412ebaf716e8d5
SHUBH0924 <shubhjeetpaul@gmail.com>
2024-07-05 12:09:54 +0530

    sorting

--- bubblesort.py
+++ bubblesort.py
@@ -1,7 +1,8 @@
 def sort(arr):
     """Sorts an array of integers in ascending order using the bubble sort algorithm."""
-    for i in range(1,len(arr)):
-        for j in range(0,len(arr)-i):
+    n=len(arr)
+    for i in range(1,n):
+        for j in range(0,n-i):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
